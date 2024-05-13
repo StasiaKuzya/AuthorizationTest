8dd07176823e32329ac150ccc46b125c603b852b
StasiaKuzya <kuzina-sta@mail.ru>
2024-05-13 11:34:37 +0300

    Initial Commit

---
+++ AuthorizationTest/AuthorizationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthorizationTestApp.swift
+//  AuthorizationTest
+//
+//  Created by Анастасия on 13.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct AuthorizationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
